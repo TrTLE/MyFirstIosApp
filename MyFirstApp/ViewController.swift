1a5d9531570d4091cdaca642fe3a542e5894d767
admin <chpaqou@gmail.com>
2019-05-20 11:15:52 +0200

    Commit du 19-20-05 11:15
    Création du projet et affiche coucou lors du clip sur le bouton

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MonTexte: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func MonBoutonClique() {
+        MonTexte.text = "COUCOU"
+        
+    }
+    
 
 }
 
